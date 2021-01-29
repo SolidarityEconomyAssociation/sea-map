e1ee3ecf2ba148efa0cd8bdb5a8a47b04dc86367
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-01-29 23:28:40 +0000

    www/map-app/app.js - load config at run-time, invoke main

--- www/map-app/app.js
+++ www/map-app/app.js
@@ -1,5 +1,14 @@
 "use strict";
 
-module.exports = require("./app/main");
-console.log("app/main.js has been loaded");
-
+const main = require("./app/main");
+const config = fetch("/configuration/config.json");
+const versions = fetch("/configuration/version.json");
+const about = fetch("/configuration/about.html");
+const getJson = (r) => r.json();
+const getText = (r) => r.text();
+Promise
+  .all([config.then(getJson), versions.then(getJson), about.then(getText)])
+  .then(([config, versions, about]) => {
+    const combinedConfig = { ...config, ...versions, aboutHtml: about };
+    main(combinedConfig);
+  });
